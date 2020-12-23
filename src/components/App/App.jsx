30d68fabd712f891f1ed6d4520b17485035a943f
Olha Hrymchak <forwork1177@gmail.com>
2020-12-23 10:38:59 +0200

    save before refactoring

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -14,36 +14,28 @@ export const App = () => {
       const sub = source$.subscribe(setCurrentTime);
       return () => sub.unsubscribe();
     }
-
-    console.log(count, 'count');
   }, [count]);
 
   const getCreateTime = () => {
-    console.log('getCreateTime');
     const createTime = new Date(currentTime * 1000).toISOString().substr(11, 8); 
-    console.log(createTime, 'createTime');
     return createTime;
   };
 
   getCreateTime();
 
   const startTimer = () => {
-    console.log('startTimer');
     source$.subscribe(setCountTime(true));
   };
 
   const stopTimer = () => {
-    console.log('stopTimer');
     source$.subscribe(setCountTime(false));
   };
 
   const waitTimer = () => {
-    console.log('waitTimer');
     source$.subscribe(setCountTime(false));
   };
 
   const resetTimer = () => {
-    console.log('resetTimer');
     source$.subscribe(setCurrentTime(0));
     source$.subscribe(setCountTime(false));
   };
